acbc73a699296be2abc9962274fc144816232033
gzmask <gzmask@hotmail.com>
2013-01-15 18:35:56 -0600

    first

---
+++ app.js
@@ -0,0 +1,43 @@
+// My SocketStream 0.3 app
+
+var http = require('http'),
+    ss = require('socketstream');
+
+// Define a single-page client called 'main'
+ss.client.define('main', {
+  view: 'app.html',
+  css:  ['libs/reset.css', 'app.styl'],
+  code: ['libs/jquery.min.js', 'app'],
+  tmpl: '*'
+});
+
+ss.client.define('index', {
+  view: 'index.html',
+  css:  ['libs/reset.css', 'style.css'],
+  code: ['libs/jquery.min.js', 'app'],
+  tmpl: '*'
+});
+
+// Serve this client on the root URL
+ss.http.route('/', function(req, res){
+  res.serveClient('index');
+});
+
+// Code Formatters
+ss.client.formatters.add(require('ss-stylus'));
+
+// Use server-side compiled Hogan (Mustache) templates. Others engines available
+ss.client.templateEngine.use(require('ss-hogan'));
+
+// Minimize and pack assets if you type: SS_ENV=production node app.js
+if (ss.env === 'production') ss.client.packAssets();
+
+// Start web server
+var server = http.Server(ss.http.middleware);
+if (ss.env === 'production')
+  server.listen(80);
+else 
+  server.listen(3000);
+
+// Start SocketStream
+ss.start(server);
