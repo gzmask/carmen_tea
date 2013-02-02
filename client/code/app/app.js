c9ef8f5d21cfee315b6b61c25518eed928890b7f
gzmask <gzmask@hotmail.com>
2013-02-02 16:19:16 -0600

    added copyright, change top picture size

--- client/code/app/app.js
+++ client/code/app/app.js
@@ -30,3 +30,16 @@ $('.nav a').click(function(e) {
   $('body').css('background-image', $(section).data('body_bg'));
   $(this).parent().addClass('active');
 });
+
+$('#bt_box').hide();
+$('#dt_box').hide();
+$('#pt_box').hide();
+$('#bt_c').click(function(e) {
+  $('#bt_box').toggle('slow');
+});
+$('#dt_c').click(function(e) {
+  $('#dt_box').toggle('slow');
+});
+$('#pt_c').click(function(e) {
+  $('#pt_box').toggle('slow');
+});
